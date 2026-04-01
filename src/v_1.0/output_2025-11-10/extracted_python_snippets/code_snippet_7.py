2a88c2bc47132cf27c7d06ba6e856a30fd64b76f
Benedek Kaibas <82393336+benedekaibas@users.noreply.github.com>
2026-04-01 18:06:19 -0400

    Delete src directory

--- src/v_1.0/output_2025-11-10/extracted_python_snippets/code_snippet_7.py
+++
@@ -1,9 +0,0 @@
-from typing import Final
-
-class Base:
-    @property
-    def value(self) -> int:
-        return 42
-
-class Derived(Base):
-    value: Final[int] = 100  # Checkers disagree on overriding property with attribute
